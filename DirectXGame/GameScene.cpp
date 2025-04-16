03717858852950f319e53761049721b08e63f37e
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-04-16 14:17:18 +0900

    01_03

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,18 +1,56 @@
 #include "GameScene.h"
 
 using namespace KamataEngine;
-//初期化
+
+// 初期化
 void GameScene::Initialize() 
 {
+	textureHandle_ = TextureManager::Load("uvChecker.png");
 
-}
-//更新
+	model_ = KamataEngine::Model::Create();
+
+	worldTransform_.Initialize();
+
+	camera_.Initialize();
+
+	}
+// 更新
 void GameScene::Update() 
 {
 
+
+	
+
 }
-//描画
+// 描画
 void GameScene::Draw() 
 {
+	/*KamataEngine::Sprite::PreDraw(KamataEngine::DirectXCommon::GetInstance()->GetCommandList());
+
+	sprite_->Draw();
+	
+	KamataEngine::Sprite::PostDraw();*/
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+
+	Model::PostDraw();
+
+	/*KamataEngine::Model::PreDraw(KamataEngine::DirectXCommon::GetInstance()->GetCommandList());
+	
+	model_->Draw(worldTransform_, debugCamera_->GetCamera(), textureHandle_);
+	KamataEngine::Model::PostDraw();*/
+}
+
+// デストラクタ
+GameScene::~GameScene() 
+{
+	delete model_;
+
+	//delete sprite_;
+
 
 }
